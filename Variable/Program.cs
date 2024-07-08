a74d053b5f15346668a890f44c561cb06ed1c15e
DreaMeR <DreaMeR@DREAMER>
2024-07-08 11:23:04 +0600

    Добавьте файлы проекта.

---
+++ Variable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 21;
+            int friendAge = 20;
+            int money = 100;
+            int entryCost = 200;
+
+            bool accesInClub = false;
+
+            char charOnFloor = 'X';
+
+            string nameOfClub = "DreamTown";
+            string name = "Peter";
+            string nameOfFriend = "Vasya";
+
+            float runSpeed = 6.2f;
+        }
+    }
+}
